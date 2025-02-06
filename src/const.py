05f2464417166a4167a5505868bae9dc75896e88
Alex Hortua <alexander@datzsolutions.com>
2025-02-05 20:48:14 -0800

    Organizing Code and including different tiles

---
+++ src/const.py
@@ -0,0 +1,39 @@
+examples=[
+    [
+        "public/samples/colToCan.webp",
+        128,
+        "Humans",
+        "Original Size"
+    ],
+    [
+        "public/samples/colToCan.webp",
+        128,
+        "Humans",
+        "Resize to Grid Size"
+    ],
+    [
+        "public/samples/forest.webp",
+        128,
+        "Robots",
+        "Original Size"
+    ],
+    [
+        "public/samples/forest.webp",
+        128,
+        "Robots",
+        "Resize to Grid Size"
+    ],
+    [
+        "public/samples/waving-colorful-flag-canada-national-600nw-1484289266.webp",
+        128,
+        "Cats",
+        "Original Size"
+    ],
+    [
+        "public/samples/waving-colorful-flag-canada-national-600nw-1484289266.webp",
+        128,
+        "Cats",
+        "Resize to Grid Size"
+    ]
+    
+]
